46370141bd020cd1ede0c49a48f2603a03790d21
Diego Borrero <die.9511@gmail.com>
2018-10-21 02:45:56 -0500

    Toolbar added

--- app/src/main/java/com/pulseyourlife/Controller/Login.java
+++ app/src/main/java/com/pulseyourlife/Controller/Login.java
@@ -1,7 +1,9 @@
 package com.pulseyourlife.Controller;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.pulseyourlife.R;
 
@@ -11,5 +13,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
