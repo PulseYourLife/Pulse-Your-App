fb538040d0a7f8e426c99b65cc10c5ddd7f3c89c
Diego Borrero <die.9511@gmail.com>
2018-12-02 21:05:10 -0500

    About us implemented

--- app/src/main/java/com/pulseyourlife/controller/About.java
+++ app/src/main/java/com/pulseyourlife/controller/About.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.pulseyourlife.R;
 
@@ -20,7 +22,16 @@ public class About extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        final Button button = (Button) getView().findViewById(R.id.button_rate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Function not implemented yet", Toast.LENGTH_SHORT).show();
+            }
+        });
         //TextView textView = (TextView) getView().findViewById(R.id.textView);
         //textView.setText(getString(R.string.login_name));
     }
+
+
 }
