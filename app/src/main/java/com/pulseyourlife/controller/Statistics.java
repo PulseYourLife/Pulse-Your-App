6335593ef520c530b884a160b2b804f4e648ce06
David Vaca <spm1dh@gmail.com>
2018-12-04 17:51:06 -0500

    Imports correction

--- app/src/main/java/com/pulseyourlife/controller/Statistics.java
+++ app/src/main/java/com/pulseyourlife/controller/Statistics.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.google.gson.Gson;
